735c352f0d1834e0341d471d1a060c33ce15cccc
bax082024 <bax082024@gmail.com>
2024-12-31 23:56:37 +0100

    added title label

--- VisualAESForm.Designer.cs
+++ VisualAESForm.Designer.cs
@@ -32,25 +32,28 @@
             textBox2 = new TextBox();
             button1 = new Button();
             btnExit = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            lblTitle = new Label();
             SuspendLayout();
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(331, 169);
+            textBox1.Location = new Point(137, 165);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(100, 23);
+            textBox1.Size = new Size(158, 23);
             textBox1.TabIndex = 0;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(330, 209);
+            textBox2.Location = new Point(137, 221);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(100, 23);
+            textBox2.Size = new Size(159, 23);
             textBox2.TabIndex = 1;
             // 
             // button1
             // 
-            button1.Location = new Point(290, 292);
+            button1.Location = new Point(97, 440);
             button1.Name = "button1";
             button1.Size = new Size(75, 23);
             button1.TabIndex = 2;
@@ -62,7 +65,7 @@
             btnExit.BackColor = Color.Silver;
             btnExit.FlatStyle = FlatStyle.Popup;
             btnExit.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btnExit.Location = new Point(882, 631);
+            btnExit.Location = new Point(394, 512);
             btnExit.Name = "btnExit";
             btnExit.Size = new Size(75, 23);
             btnExit.TabIndex = 3;
@@ -70,11 +73,42 @@
             btnExit.UseVisualStyleBackColor = false;
             btnExit.Click += btnExit_Click;
             // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(50, 162);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 4;
+            label1.Text = "label1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(50, 224);
+            label2.Name = "label2";
+            label2.Size = new Size(38, 15);
+            label2.TabIndex = 5;
+            label2.Text = "label2";
+            // 
+            // lblTitle
+            // 
+            lblTitle.AutoSize = true;
+            lblTitle.Font = new Font("Segoe UI", 27.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblTitle.Location = new Point(162, 9);
+            lblTitle.Name = "lblTitle";
+            lblTitle.Size = new Size(204, 50);
+            lblTitle.TabIndex = 6;
+            lblTitle.Text = "Visual AES";
+            // 
             // VisualAESForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(969, 666);
+            ClientSize = new Size(573, 615);
+            Controls.Add(lblTitle);
+            Controls.Add(label2);
+            Controls.Add(label1);
             Controls.Add(btnExit);
             Controls.Add(button1);
             Controls.Add(textBox2);
@@ -91,5 +125,8 @@
         private TextBox textBox2;
         private Button button1;
         private Button btnExit;
+        private Label label1;
+        private Label label2;
+        private Label lblTitle;
     }
 }
